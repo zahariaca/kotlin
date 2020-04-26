1cef98ee18db9a4c8a08bc57de4eed4c3e1c45cc
zahariaca <zaharia.c.alexandru@gmail.com>
2020-04-26 13:41:57 +0300

    Finised sections 1-4 of Kotlin for Java Developers udemy course

---
+++ Challenge2/src/com/zahariaca/javacode/DummyClass.java
@@ -0,0 +1,9 @@
+package com.zahariaca.javacode;
+
+public class DummyClass {
+    public void method1(char[] charArray) {
+        for (char c : charArray) {
+            System.out.println(c);
+        }
+    }
+}
